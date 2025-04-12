5074d9d36e461bc3d0f382d8b2086d4baf66f4b6
Sonali Sidana <sonalisidana13@gmail.com>
2025-04-12 23:45:00 +0530

    added conditional export

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,12 @@ const withMDX = mdx({
   extension: /\.mdx?$/,
   options: {},
 });
+const isExport = process.env.EXPORT === 'true';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
-  output: "export",
+  ...(isExport && { output: 'export' }),
 };
 
 export default withMDX(nextConfig);
